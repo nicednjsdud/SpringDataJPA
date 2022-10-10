fa9bb9387d1a2000c2ab108be7bbdfc365f40142
JeongWonYoung <nicednjsdud@naver.com>
2022-10-11 01:01:14 +0900

    Test returnType
    to study return type

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -150,4 +150,18 @@ class MemberRepositoryTest {
             System.out.println("member = " + member);
         }
     }
+    
+    @Test
+    public void returnType(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findListByUsername("AAA");
+        Member findMember = memberRepository.findMemberByUsername("AAA");
+        Optional<Member> findMember2 = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("findMember = " + findMember);
+        
+    }
 }

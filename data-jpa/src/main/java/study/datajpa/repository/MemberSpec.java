c1db830f9969b19874f4e4137c0e99dc81cb3464
JeongWonYoung <nicednjsdud@naver.com>
2022-10-12 21:39:23 +0900

    Add MemberSpec
    speicification use

---
+++ data-jpa/src/main/java/study/datajpa/repository/MemberSpec.java
@@ -0,0 +1,26 @@
+package study.datajpa.repository;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
+
+import javax.persistence.criteria.*;
+
+public class MemberSpec {
+
+    public static Specification<Member> teamName(final String teamName) {
+        return (Specification<Member>) (root, query, criteriaBuilder) -> {
+            if (StringUtils.isEmpty(teamName)) {
+                return null;
+            }
+            Join<Member, Team> t = root.join("team", JoinType.INNER);// 회원과 조인
+            return criteriaBuilder.equal(t.get("name"), teamName);
+        };
+    }
+
+    public static Specification<Member> username(final String userName) {
+        return (Specification<Member>) (root, query, criteriaBuilder) ->
+                criteriaBuilder.equal(root.get("username"), userName);
+    }
+}
